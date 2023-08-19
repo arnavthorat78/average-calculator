a3b43ee3a1f71f9782861da7429e822e35f03522
arnavthorat78 <arnavthorat78@gmail.com>
2023-08-19 12:31:20 +1000

    Completed application

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const numbers = document.querySelector("numbers");
-const calculateBtn = document.querySelector("calculate");
-const result = document.querySelector("result");
+const numbers = document.querySelector(".numbers");
+const calculateBtn = document.querySelector(".calculate");
+const error = document.querySelector(".error");
+const result = document.querySelector(".result");
 
 calculateBtn.addEventListener("click", () => {
-  document.querySelector("test").innerText = "hello"
-})
+  result.value = "";
+
+  if (numbers.value === "") {
+    error.innerHTML = "Please enter numbers in the textbox above.";
+    return;
+  }
+
+  const numValues = String(numbers.value)
+    .split("\n")
+    .map((value) => parseFloat(value));
+
+  if (numValues.includes(NaN)) {
+    error.innerHTML = "Only numbers are accepted in the textbox!";
+    return;
+  }
+
+  error.innerHTML = "";
+
+  const sum = numValues.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+
+  result.value = (sum / numValues.length).toFixed(2);
+});
